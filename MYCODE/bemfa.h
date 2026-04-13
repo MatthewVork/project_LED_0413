125432786c56b3c81cd149d912b4b077e23ee271
MatthewVork <wzy1845089791@gmail.com>
2026-04-13 20:32:27 +0800

    添加了WIFI模块和巴法云的逻辑，等待完善

---
+++ MYCODE/bemfa.h
@@ -0,0 +1,7 @@
+#ifndef _BEMFA_H_
+#define _BEMFA_H_
+
+#include "stm32f4xx.h"
+#include "delay.h"
+
+#endif
---
+++ MYCODE/esp8266.h
@@ -0,0 +1,6 @@
+#ifndef __ESP8266_H_
+#define __ESP8266_H_
+
+#include "stm32f4xx.h"
+
+#endif
